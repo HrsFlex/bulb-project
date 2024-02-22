e0b6835c149ced08bfbbe427db36b674e8defa03
Harsh Kumar <116896176+HrsFlex@users.noreply.github.com>
2024-02-22 17:21:49 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+function onBulb(){
+    var bulbImage = document.getElementById("bulbImage");
+    bulbImage.src = 'onBulb.jpg';
+}
+
+function offBulb(){
+    var bulbImage = document.getElementById("bulbImage");
+    bulbImage.src = "ofBulb.jpg";
+}
